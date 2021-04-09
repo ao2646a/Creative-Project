0c2314021a0ec0bbbd8db4faa47fcdba538786dd
Arianna O'Brien Cannon <ao2646a@american.edu>
2021-04-09 10:41:19 -0400

    Add files via upload

--- CreativeProject/creativeproject.js
+++ CreativeProject/creativeproject.js
@@ -2,6 +2,11 @@
     "use strict";
     window.onload = function () {
         document.getElementById("start").onclick = form1;
+        document.getElementById("back1").onclick = back1;
+        
+        document.getElementById("next1").onclick = next1;
+        
+        document.getElementById("back2").onclick = back2;
     }
     
     
@@ -9,6 +14,24 @@
         document.querySelector("#main-view").classList.add("hidden");
         document.querySelector("#form1").classList.remove("hidden");
         document.querySelector("body").style.backgroundImage = "none";
+    }
+    
+    function back1 (){
+         document.querySelector("#main-view").classList.remove("hidden");
+        document.querySelector("#form1").classList.add("hidden");
+         document.querySelector("body").style.backgroundImage = "url(college-students.jpeg)";
+    }
+    
+    function next1 (){
+        document.querySelector("#form1").classList.add("hidden");
+        document.querySelector("#form2").classList.remove("hidden");
+        document.querySelector("body").style.backgroundImage = "none";
+    }
+    
+    function back2 (){
+         document.querySelector("#form1").classList.remove("hidden");
+        document.querySelector("#form2").classList.add("hidden");
+         
     }
     
 })();
